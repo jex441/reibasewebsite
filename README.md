cfd08630de669f7a0c904e4ece7655e54306ee93
Jeffrey Wood <jwfirenze@gmail.com>
2022-06-02 15:45:03 -0400

    pr

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# reibasewebsite
-
 # scripts
 
 ## npm start
--- server/index.js
+++ server/index.js
@@ -5,5 +5,5 @@ const port = 3000;
 const app = require("./app");
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Reibase App listening on port ${port}`);
 });
