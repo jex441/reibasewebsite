f6653947aacb3b0d9f14b43affcf9dd0a61ace85
Jeffrey Wood <jwfirenze@gmail.com>
2022-06-25 10:56:14 -0400

    https redirect

--- server/app.js
+++ server/app.js
@@ -3,6 +3,14 @@ const app = express();
 const path = require("path");
 module.exports = app;
 
+if (process.env.NODE_ENV === "production") {
+  app.use((req, res, next) => {
+    if (req.header("x-forwarded-proto") !== "https")
+      res.redirect(`https://${req.header("host")}${req.url}`);
+    else next();
+  });
+}
+
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("*", function (req, res) {
